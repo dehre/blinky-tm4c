75ffcad9648286f77da4704d4e5ec9f2c7f232a0
L-oris <loris4526@gmail.com>
2021-07-28 16:19:26 +0200

    add module cycles-counter

---
+++ include/cycles-counter.h
@@ -0,0 +1,39 @@
+//*****************************************************************************
+//
+// Utility functions to collect the number of elapsed clock cycles
+// between function calls. It uses SysTick.
+//
+// Usage:
+// ```c
+// uint32_t clockCyclesDump[200] = {};
+//
+// CyclesCounter_InitSysTick();
+// CyclesCounter_Reset();
+//
+// while (1)
+// {
+//     DoSomething();
+//     CyclesCounter_Push(clockCyclesDump, sizeof(clockCyclesDump) / sizeof(uint32_t));
+// }
+// ```c
+//
+//*****************************************************************************
+
+#include <stdint.h>
+
+//
+// Initialize SysTick.
+//
+void CyclesCounter_InitSysTick(void);
+
+//
+// Reset the counter.
+//
+void CyclesCounter_Reset(void);
+
+//
+// Collect the number of clock cycles elapsed from the last
+// call to `CyclesCounter_Push` or `CyclesCounter_Reset`.
+// Returns 1 if the buffer is full.
+//
+uint8_t CyclesCounter_Push(uint32_t buffer[], uint32_t bufferLen);
---
+++ utils/cycles-counter.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <driverlib/systick.h>
+#include "inc/tm4c123gh6pm.h"
+
+#define SYSTICK_PERIOD_MAX 16777216
+
+void CyclesCounter_InitSysTick(void)
+{
+    SysTickPeriodSet(SYSTICK_PERIOD_MAX);
+    SysTickEnable();
+}
+
+void CyclesCounter_Reset(void)
+{
+    // Any write to this register clears the SysTick counter to 0.
+    NVIC_ST_CURRENT_R = 0;
+}
+
+uint8_t CyclesCounter_Push(uint32_t buffer[], uint32_t bufferLen)
+{
+    static uint32_t idx = 0;
+    if (idx > bufferLen)
+        return 1;
+
+    buffer[idx] = SYSTICK_PERIOD_MAX - SysTickValueGet();
+    idx++;
+
+    CyclesCounter_Reset();
+    return 0;
+}
